c7117a614666240e3322b87f8364e2cefe0732e4
Vasileva Evgeniya <vasilevazhenechkac@mail.ru>
2025-02-02 15:53:19 +0500

    Изменена основная логика Добавлен класс коробки

--- src/main/java/com/stepup/MainApplication.java
+++ src/main/java/com/stepup/MainApplication.java
@@ -3,6 +3,9 @@ package com.stepup;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("111111");
+        System.out.println("1131111");
+    }
+    public static void doSomething () {
+        System.out.println(1);
     }
 }
